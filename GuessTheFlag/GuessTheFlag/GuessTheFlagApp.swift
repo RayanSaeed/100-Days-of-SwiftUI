d48bf2252d0718e011da6bc2cb2fdf285f27ed5c
Rayan Saeed <rayancodes@gmail.com>
2022-04-20 15:05:04 +0500

    Add GuessTheFlag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Rayan Saeed on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
